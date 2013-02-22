ead54effbd13cc4166249ba2aaaf3d9737c7e921
Pardis <pardis@pardis-VirtualBox.(none)>
2013-02-22 16:30:06 +0100

    prova

---
+++ JAVA.java
@@ -0,0 +1,22 @@
+/*
+ * Nome:untitled.java
+ * Package: 
+ * Autore:
+ * Data:
+ * Versione:
+ * 
+ * Modifiche:
+ * +------+---------------+-----------+
+ * | Data | Programmatore | Modifiche |
+ * +------+---------------+-----------+
+ * |      |               |           |
+ */
+
+
+public class untitled {
+	
+	public static void main (String args[]) {
+		System.out.println("");
+	}
+}
+
