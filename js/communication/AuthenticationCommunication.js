3a832fd3e23916a87bc4d5f8f99b9b3da44171d0
Pardis <pardis@pardis-VirtualBox.(none)>
2013-02-28 15:26:10 +0100

    cartelle

---
+++ js/communication/AuthenticationCommunication.js
@@ -0,0 +1,19 @@
+/*
+ * Nome:AuthenticationCommunication.js
+ * Package: 
+ * Autore:
+ * Data:
+ * Versione:
+ * 
+ * Modifiche:
+ * +------+---------------+-----------+
+ * | Data | Programmatore | Modifiche |
+ * +------+---------------+-----------+
+ * |      |               |           |
+ */
+
+function AuthenticationCommunication(){
+	
+	
+	
+}
---
+++ js/view/ContactsView.js
@@ -0,0 +1,14 @@
+/*
+ * Nome:untitled.js
+ * Package: 
+ * Autore:
+ * Data:
+ * Versione:
+ * 
+ * Modifiche:
+ * +------+---------------+-----------+
+ * | Data | Programmatore | Modifiche |
+ * +------+---------------+-----------+
+ * |      |               |           |
+ */
+
