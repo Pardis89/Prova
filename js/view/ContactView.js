84f7a63b486fd9a79dbf3e530e8713c805864f1c
Liquid90 <g.bain@libero.it>
2013-03-06 10:06:38 +0100

    Aggiornamenti vari

--- js/view/ContactView.js
+++ js/view/ContactView.js
@@ -17,22 +17,27 @@ define([
  'underscore',  
  'backbone',
  'text!templates/ContactTemplate.html',
- 'view/FunctionsView'
-], function($, _, Backbone, ContactTemplate, FunctionsView){
+ 'view/FunctionsView',
+ 'view/ChatView',
+ 'model/TextMessageModel'
+], function($, _, Backbone, ContactTemplate, FunctionsView, ChatView, TextMessageModel){
  var ContactView = Backbone.View.extend({
     template: _.template(ContactTemplate),
     
     events:{
 		"click li.contact" : "view"
 	},
+	var this.TextMessageModel=new TextMessageModel();
 //Per ora rendo sempre visibili dei contatti:
   render: function(){
     this.$el.html(this.template({dom: this.options.dom, username: this.model.toJSON().username}));   
 	return this;
   },
 // Su temp
-	view : function(ContactModel){
+	view : function(){
 		var fview= new FunctionsView({model: this.model});
+		
+		var chat=new ChatView({model: this.TextMessageModel});
 	}
 
 }); 
